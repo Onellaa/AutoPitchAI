3f6eb7ac5edec1407934f28d925112c91df79e78
Onellaa <onelladias17@gmail.com>
2025-05-27 17:26:55 +0530

    Initial commit

---
+++ backend/src/main/java/com/autopitchai/backend/AutoPitchAiApplication.java
@@ -0,0 +1,13 @@
+package com.autopitchai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPitchAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPitchAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/autopitchai/backend/AutoPitchAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopitchai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPitchAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
